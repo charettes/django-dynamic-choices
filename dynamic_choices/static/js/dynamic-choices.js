0763df8e8e87efbfe19bdcbb179269b9db213e16
Simon Charette <charette.s@gmail.com>
2010-12-12 01:12:29 -0500

    Make sure fields bound to fields that require updating are update right after

--- dynamic_choices/static/js/dynamic-choices.js
+++ dynamic_choices/static/js/dynamic-choices.js
@@ -1,5 +1,8 @@
 (function($){
 
+  var DATA_BOUND_FIELDS = 'data-dynamic-choices-bound-fields',
+      DATA_FORMSET = 'data-dynamic-choices-formset';
+
   var error = (function(){
     if ('console' in window && $.isFunction(console.error))
       return function(e) { // We must wrap the function see 
@@ -21,7 +24,16 @@
     if (this.length) {
       form = $(form ? form : this[0].form);
       var fields = $(this).addClass('loading'),
-          data = $(form).serializeArray();
+          data = $(form).serializeArray(),
+          boundFields = [];
+      // Make sure fields bound to these ones are updated right after
+      fields.each(function(i, field){
+        var selector = $(field).attr(DATA_BOUND_FIELDS);
+        if (selector) boundFields.push(selector);
+        var formset = $(field).closest('[' + DATA_FORMSET + ']').attr(DATA_FORMSET);
+        if (formset) boundFields.push($.fn.bindFieldset.formsetsBindings[formset](field));
+      });
+      fields.addClass('loading');
       data.push({name: 'DYNAMIC_CHOICES_FIELDS',
                 value:getFieldNames(fields).join(',')});
       $.getJSON(url, $.param(data), function(json){
@@ -34,6 +46,7 @@
           }
           $(field).removeClass('loading');
         });
+        $(boundFields.join(', ')).updateFields(url, form);
       });
     }
     return this;
@@ -68,12 +81,12 @@
     return this.each(function(index, field){
       $(field).change(function(){
         $(fields).updateFields(url, field.form);
-      });
+      }).attr(DATA_BOUND_FIELDS, fields);
     });
   };
   
   function defaultFieldNameExtractor(fieldset, field) {
-    var match = field.match(/^([\w_]+)-(\d+)-([\w_]+)$/);
+    var match = field.match(/^([\w_]+)-(\w+)-([\w_]+)$/);
     if (match && match[1] == fieldset) {
       return {
         index: match[2],
@@ -92,19 +105,28 @@
     };
   };
   
+  function formsetFieldBoundFields(fieldset, field, fields, extractor, builder) {
+    field = extractor(fieldset, field.name);
+    if (field.name in fields) {
+      var selectors = $(fields[field.name]).map(curryBuilder(fieldset, field.index, builder));
+      return selectors.toArray().join(', ');
+    } else return '';
+  };
+  
   $.fn.bindFieldset = function(url, fieldset, fields, extractor, builder) {
     extractor = $.isFunction(extractor) ? extractor : defaultFieldNameExtractor;
     builder = $.isFunction(builder) ? builder : defaultFieldSelectorBuilder;
+    $.fn.bindFieldset.formsetsBindings[fieldset] = function(field){
+      return formsetFieldBoundFields(fieldset, field, fields, extractor, builder);
+    };
     return this.each(function(index, container){
       $(container).change(function(event){
         var target = event.target,
-            field = extractor(fieldset, target.name);
-        if (field.name in fields) {
-          var selectors = $(fields[field.name]).map(curryBuilder(fieldset, field.index, builder));
-          $(selectors.toArray().join(', ')).updateFields(url, target.form);
-        }
-      });
+            selectors = formsetFieldBoundFields(fieldset, target, fields, extractor, builder);
+        $(selectors).updateFields(url, target.form);
+      }).attr(DATA_FORMSET, fieldset);
     });
   };
+  $.fn.bindFieldset.formsetsBindings = {}
 
 })(jQuery || django.jQuery);

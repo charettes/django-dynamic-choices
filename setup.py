aa5fbf23f1f61358e54b3d2282d42cf443f3bc73
Simon Charette <charette.s@gmail.com>
2010-12-26 17:10:28 -0500

    Fixed setup.py in order to distribute via pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Dynamic admin',
+    name='django-dynamic-choices',
     version='0.1',
-    description='Django admin dynamizer',
-    long_description='Provide FK and M2M dynamic choices and react to django.contrib.admin edit/add view interaction to update those choices',
+    description='Django admin fk and m2m dynamic choices by providing callback support',
+    long_description='Provide fk and m2m dynamic choices and react to django.contrib.admin edit/add view interaction to update those choices',
+    url='https://github.com/charettes/django-dynamic-choices',
     author='Simon Charette',
     author_email='charette.s@gmail.com',
-    packages = [ #TODO:
-    ]
+    install_requires=[
+        'Django>=1.2',
+    ],
+    packages=find_packages(exclude=['testapp']),
+    include_package_data=True
 )

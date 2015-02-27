23852f865e53ae940db788d8b45ff42502c60858
Simon Charette <charette.s@gmail.com>
2015-02-27 16:36:18 -0500

    Cleaned up the JavaScript.

--- dynamic_choices/static/js/dynamic-choices-admin.js
+++ dynamic_choices/static/js/dynamic-choices-admin.js
@@ -1,175 +1,187 @@
-($ || jQuery || django.jQuery)(document).ready(function(){
+($ || jQuery || django.jQuery)(function($) {
+    var assignOptions = $.fn.updateFields.widgetHandlers['default'];
 
-  var $ = ($ || jQuery || django.jQuery);
+    var filteredSelectMultiple = 'django.contrib.admin.widgets.FilteredSelectMultiple';
+    $.fn.updateFields.widgetHandlers[filteredSelectMultiple] = function(field, values) {
+        var chosenField = $(field),
+            alreadyChosens = $(field.options).map(function(index, element) {
+                return element.value;
+            }),
+            availableField = $('#id_' + field.name + '_from'),
+            fromCache = [],
+            toCache = [],
+            availables = [],
+            chosens = [];
 
-  var assignOptions = $.fn.updateFields.widgetHandlers['default'];
+        //The widget isn't initialized yet (it might a be an inline empty template)
+        if (availableField.length == 0) {
+            return assignOptions(chosenField, values);
+        }
 
-  var filteredSelectMultiple = 'django.contrib.admin.widgets.FilteredSelectMultiple';
-  $.fn.updateFields.widgetHandlers[filteredSelectMultiple] = function(field, values) {
-    var chosenField = $(field),
-        alreadyChosens = $(field.options).map(function(index, element){
-          return element.value;
-        }),
-        availableField = $('#id_' + field.name + '_from'),
-        fromCache = [],
-        toCache = [],
-        availables = [], chosens = [];
-        
-    //The widget isn't initialized yet (it might a be an inline empty template)
-    if (availableField.length == 0) {
-    	return assignOptions(chosenField, values);
-    }
+        SelectBox.cache['id_' + field.name + '_from'] = [];
+        SelectBox.cache['id_' + field.name + '_to'] = [];
 
-    SelectBox.cache['id_' + field.name + '_from'] = [];
-    SelectBox.cache['id_' + field.name + '_to'] = [];
+        availableField.empty();
+        chosenField.empty();
 
-    availableField.empty();
-    chosenField.empty();
-    
-    $(values).each(function(index, value){
-      //We cast the value to string since the "type" is lost when retreiving
-      //from option.value by SelectBox
-      var chosen = $.inArray(String(value[0]), alreadyChosens) != -1; 
-      (chosen ? chosens : availables).push(value);
-      (chosen ? toCache : fromCache).push({value: value[0], text: value[1], displayed: 1});
-    });
-    
-    SelectBox.cache['id_' + field.name + '_from'] = fromCache;
-    SelectBox.cache['id_' + field.name + '_to'] = toCache;
-    
-    assignOptions(availableField, availables);
-    assignOptions(chosenField, chosens);
-  };
+        $(values).each(function(index, value) {
+            //We cast the value to string since the "type" is lost when retreiving
+            //from option.value by SelectBox
+            var chosen = $.inArray(String(value[0]), alreadyChosens) != -1;
+            (chosen ? chosens : availables).push(value);
+            (chosen ? toCache : fromCache).push({
+                value: value[0],
+                text: value[1],
+                displayed: 1
+            });
+        });
 
-  django.dynamicAdmin = function(fields, inlines){
-  	var url = document.location.pathname + 'choices/';
-    $(document).ready(function(){
-      for (f in fields) {
-        $(f).bindFields(url, fields[f].join(', '));
-      }
-      for (f in inlines) {
-        $('#' + f + '-group').bindFieldset(url, f, inlines[f]);
-      }
-    });
-  };
-  
-  var DATA_ORIGINAL_HREF = 'data-original-href';
-  
-  function getAddLink(element) {
-  	var addLink = $('#add_' + element.id);
-  	if (!addLink.length) throw new Error('Cannot find add link of field ' + element.id);
-  	return addLink;
-  };
-  
-  function prepareAddLink(element) {
-  	var addLink = getAddLink(element);
-		addLink.attr(DATA_ORIGINAL_HREF, addLink.attr('href'));
-  };
-  
-  function updateAddLink(element, fields, fieldsetFields, parametersCallback) {
-  	var addLink = getAddLink(element),
-  			parameters = {},
-  			encodedParameters = [];
-  	
-  	$(fields).each(function(index, field){
-  		var name = $(field).attr('name'),
-  				value = $(field).val();
-  		if (value) parameters[name] = value;
-  	});
-  	$(fieldsetFields).each(function(index, field){
-  		var name = inlineField(field).name,
-  				value = $(field).val();
-  		if (value) parameters[name] = value;
-  	});
-		
-		if ($.isFunction(parametersCallback)) parameters = parametersCallback(parameters);
-			
-  	for (var name in parameters) {
-  		encodedParameters.push([encodeURI(name), encodeURI(parameters[name])].join('='));
-  	}
-  	
-  	$(addLink).attr('href', addLink.attr(DATA_ORIGINAL_HREF) + '?' + encodedParameters.join('&'));
-  };
-  
-  django.dynamicAdmin.bindFieldsAddLink = function(field, fields, parametersCallback) {
-  	$(field).each(function(index, element){
-  		try {
-  			prepareAddLink(element);
-  		} catch (e) {return}
-  		$(fields).change(function(){
-				updateAddLink(element, fields, null, parametersCallback);
-			});
-			updateAddLink(element, fields, null, parametersCallback);
-  	});
-  };
-  
-  function inlineField(field) {
-  	var field = $(field).attr('name').split('-');
-  	return {fieldset: field[0], index: field[1], name: field[2]}
-  };
-  
-  function buildInlineFieldSelector(fieldName) {
-  	return '[name$="' + fieldName + '"]';
-  };
-  
-  function buildInlineFieldId(formsetName, fieldName, fieldIndex) {
-  	return '#id_' + formsetName + '-' + fieldIndex + '-' + fieldName;
-  };
-  
-  function buildFormsetFieldsSelector(formsetName, fields, fieldIndex) {
-  	return $(fields).map(function(index, field){
-  		return buildInlineFieldId(formsetName, field, fieldIndex);
-  	}).toArray().join(', ');
-  };
-  
-  django.dynamicAdmin.bindFormsetFieldsAddLink = function(formset, field, bindedFormsetFields, bindedFormFieldsSelector, parametersCallback) {
-  	bindedFormsetFields = bindedFormsetFields || [];
-  	bindedFormFieldsSelector = bindedFormFieldsSelector || '';
-  	var fieldSelector = buildInlineFieldSelector(field);
-  	$(formset).each(function(index, formset){
-  		var formsetName = formset.id.match(/^(\w+)-group$/)[1]
-  		bindedFieldsetFieldsSelector = $(bindedFormsetFields)
-  																		.map(function(i, e){return buildInlineFieldSelector(e)})
-  																		.toArray().join(', ');
-  		var fields = $(formset).find(fieldSelector).map(function(index, element){
-  			try {prepareAddLink(element)} catch(e) {return false};
-  			var index = inlineField(element).index;
-  			updateAddLink(element,
-  										bindedFormFieldsSelector,
-  										buildFormsetFieldsSelector(formsetName, bindedFormsetFields, index),
-  										parametersCallback);
-  			return true;
-  		}).toArray();
-  		if ($.inArray(true, fields) == -1) return;
-  		$(formset).find(bindedFieldsetFieldsSelector).live('change', function(event){
-  			var index = inlineField(event.target).index;
-  			updateAddLink($(buildInlineFieldId(formsetName, field, index))[0], 
-  										bindedFormFieldsSelector,
-  										buildFormsetFieldsSelector(formsetName, bindedFormsetFields, index),
-  										parametersCallback);
-  		});
-  		$(bindedFormFieldsSelector).change(function(event){
-  			$(formset).find(fieldSelector).each(function(index, element){
-  				var index = inlineField(element).index;
-  				updateAddLink(element,
-  											bindedFormFieldsSelector,
-  											buildFormsetFieldsSelector(formsetName, bindedFormsetFields, index),
-  											parametersCallback);
-  			});
-  		});
-  	});
-  };
-  
-  // Creating new instance within the admin doesn't trigger the change event on select fields.
-  // Attempt to trigger it correcly.
-	var originalDismissAddAnotherPopup = dismissAddAnotherPopup;
-  dismissAddAnotherPopup = function(win, newId, newRepr) {
-  	originalDismissAddAnotherPopup(win, newId, newRepr);
-  	newId = html_unescape(newId);
-		newRepr = html_unescape(newRepr);
-		var id = windowname_to_id(win.name);
-		$('#' + id).trigger('change');
-  };
-  dismissAddAnotherPopup.original = originalDismissAddAnotherPopup;
-  
+        SelectBox.cache['id_' + field.name + '_from'] = fromCache;
+        SelectBox.cache['id_' + field.name + '_to'] = toCache;
+
+        assignOptions(availableField, availables);
+        assignOptions(chosenField, chosens);
+    };
+
+    django.dynamicAdmin = function(fields, inlines) {
+        var url = document.location.pathname + 'choices/';
+        for (f in fields) {
+            $(f).bindFields(url, fields[f].join(', '));
+        }
+        for (f in inlines) {
+            $('#' + f + '-group').bindFieldset(url, f, inlines[f]);
+        }
+    };
+
+    var DATA_ORIGINAL_HREF = 'data-original-href';
+
+    function getAddLink(element) {
+        var addLink = $('#add_' + element.id);
+        if (!addLink.length) throw new Error('Cannot find add link of field ' + element.id);
+        return addLink;
+    };
+
+    function prepareAddLink(element) {
+        var addLink = getAddLink(element);
+        addLink.attr(DATA_ORIGINAL_HREF, addLink.attr('href'));
+    };
+
+    function updateAddLink(element, fields, fieldsetFields, parametersCallback) {
+        var addLink = getAddLink(element),
+            parameters = {},
+            encodedParameters = [];
+
+        $(fields).each(function(index, field) {
+            var name = $(field).attr('name'),
+                value = $(field).val();
+            if (value) parameters[name] = value;
+        });
+        $(fieldsetFields).each(function(index, field) {
+            var name = inlineField(field).name,
+                value = $(field).val();
+            if (value) parameters[name] = value;
+        });
+
+        if ($.isFunction(parametersCallback)) parameters = parametersCallback(parameters);
+
+        for (var name in parameters) {
+            encodedParameters.push([encodeURI(name), encodeURI(parameters[name])].join('='));
+        }
+
+        $(addLink).attr('href', addLink.attr(DATA_ORIGINAL_HREF) + '?' + encodedParameters.join('&'));
+    };
+
+    django.dynamicAdmin.bindFieldsAddLink = function(field, fields, parametersCallback) {
+        $(field).each(function(index, element) {
+            try {
+                prepareAddLink(element);
+            } catch (e) {
+                return
+            }
+            $(fields).change(function() {
+                updateAddLink(element, fields, null, parametersCallback);
+            });
+            updateAddLink(element, fields, null, parametersCallback);
+        });
+    };
+
+    function inlineField(field) {
+        var field = $(field).attr('name').split('-');
+        return {
+            fieldset: field[0],
+            index: field[1],
+            name: field[2]
+        }
+    };
+
+    function buildInlineFieldSelector(fieldName) {
+        return '[name$="' + fieldName + '"]';
+    };
+
+    function buildInlineFieldId(formsetName, fieldName, fieldIndex) {
+        return '#id_' + formsetName + '-' + fieldIndex + '-' + fieldName;
+    };
+
+    function buildFormsetFieldsSelector(formsetName, fields, fieldIndex) {
+        return $(fields).map(function(index, field) {
+            return buildInlineFieldId(formsetName, field, fieldIndex);
+        }).toArray().join(', ');
+    };
+
+    django.dynamicAdmin.bindFormsetFieldsAddLink = function(formset, field, bindedFormsetFields, bindedFormFieldsSelector, parametersCallback) {
+        bindedFormsetFields = bindedFormsetFields || [];
+        bindedFormFieldsSelector = bindedFormFieldsSelector || '';
+        var fieldSelector = buildInlineFieldSelector(field);
+        $(formset).each(function(index, formset) {
+            var formsetName = formset.id.match(/^(\w+)-group$/)[1]
+            bindedFieldsetFieldsSelector = $(bindedFormsetFields)
+                .map(function(i, e) {
+                    return buildInlineFieldSelector(e)
+                })
+                .toArray().join(', ');
+            var fields = $(formset).find(fieldSelector).map(function(index, element) {
+                try {
+                    prepareAddLink(element)
+                } catch (e) {
+                    return false
+                };
+                var index = inlineField(element).index;
+                updateAddLink(element,
+                    bindedFormFieldsSelector,
+                    buildFormsetFieldsSelector(formsetName, bindedFormsetFields, index),
+                    parametersCallback);
+                return true;
+            }).toArray();
+            if ($.inArray(true, fields) == -1) return;
+            $(formset).find(bindedFieldsetFieldsSelector).live('change', function(event) {
+                var index = inlineField(event.target).index;
+                updateAddLink($(buildInlineFieldId(formsetName, field, index))[0],
+                    bindedFormFieldsSelector,
+                    buildFormsetFieldsSelector(formsetName, bindedFormsetFields, index),
+                    parametersCallback);
+            });
+            $(bindedFormFieldsSelector).change(function(event) {
+                $(formset).find(fieldSelector).each(function(index, element) {
+                    var index = inlineField(element).index;
+                    updateAddLink(element,
+                        bindedFormFieldsSelector,
+                        buildFormsetFieldsSelector(formsetName, bindedFormsetFields, index),
+                        parametersCallback);
+                });
+            });
+        });
+    };
+
+    // Creating new instance within the admin doesn't trigger the change event on select fields.
+    // Attempt to trigger it correcly.
+    var originalDismissAddAnotherPopup = dismissAddAnotherPopup;
+    dismissAddAnotherPopup = function(win, newId, newRepr) {
+        originalDismissAddAnotherPopup(win, newId, newRepr);
+        newId = html_unescape(newId);
+        newRepr = html_unescape(newRepr);
+        var id = windowname_to_id(win.name);
+        $('#' + id).trigger('change');
+    };
+    dismissAddAnotherPopup.original = originalDismissAddAnotherPopup;
+
 });
--- dynamic_choices/static/js/dynamic-choices.js
+++ dynamic_choices/static/js/dynamic-choices.js
@@ -1,136 +1,141 @@
-($ || jQuery || django.jQuery)(document).ready(function(){
+($ || jQuery || django.jQuery)(function($) {
+    var DATA_BOUND_FIELDS = 'data-dynamic-choices-bound-fields',
+        DATA_FORMSET = 'data-dynamic-choices-formset';
 
-	var $ = $ || jQuery || django.jQuery,
-			DATA_BOUND_FIELDS = 'data-dynamic-choices-bound-fields',
-      DATA_FORMSET = 'data-dynamic-choices-formset';
-      
-  var error = (function(){
-    if ('console' in window && $.isFunction(console.error))
-      return function(e) { // We must wrap the function see 
-        console.error(e);
-      };
-    else return function(e) {
-      throw new Error(e);
-    };
-  })();
+    var error = (function() {
+        if ('console' in window && $.isFunction(console.error))
+            return function(e) {
+                console.error(e);
+            };
+        else return function(e) {
+            throw new Error(e);
+        };
+    })();
 
-  function getFieldNames(fields) {
-    return fields.map(function(index, field){
-      return field.name;
-    }).toArray();
-  }
-
-  $.fn.updateFields = function(url, form) {
-    var handlers = $.fn.updateFields.widgetHandlers;
-    if (this.length) {
-      form = $(form ? form : this[0].form);
-      var fields = $(this).addClass('loading'),
-          data = $(form).serializeArray(),
-          boundFields = [];
-      // Make sure fields bound to these ones are updated right after
-      fields.each(function(i, field){
-        var selector = $(field).attr(DATA_BOUND_FIELDS);
-        if (selector) boundFields.push(selector);
-        var formset = $(field).closest('[' + DATA_FORMSET + ']').attr(DATA_FORMSET);
-        if (formset) boundFields.push($.fn.bindFieldset.formsetsBindings[formset](field));
-      });
-      fields.addClass('loading');
-      data.push({name: 'DYNAMIC_CHOICES_FIELDS',
-                value:getFieldNames(fields).join(',')});
-      $.getJSON(url, $.param(data), function(json){
-        fields.each(function(index, field){
-          if (field.name in json) {
-            var data = json[field.name];
-            if (data.widget in handlers) {
-              handlers[data.widget](field, data.value);
-              $(field).trigger('change', {'triggeredByDynamicChoices': true});
-            } else error('Missing handler for "' + data.widget + '" widget.');
-          }
-          $(field).removeClass('loading');
-        });
-        $(boundFields.join(', ')).updateFields(url, form);
-      });
+    function getFieldNames(fields) {
+        return fields.map(function(index, field) {
+            return field.name;
+        }).toArray();
     }
-    return this;
-  };
-  
-  function assignOptions(element, options) {
-    $(options).each(function(index, option) {
-      if ($.isArray(option[1])) {
-        var optGroup = $('<optgroup></optgroup>').attr({label: option[0]});
-        assignOptions(optGroup, option[1]);
-        element.append(optGroup);
-      } else {
-        element.append($('<option></option>').attr({value:option[0]}).html(option[1]));
-      }
-    });
-  }
-  
-  function selectWidgetHandler(select, options) {
-    select = $(select);
-    var value = select.val();
-    select.empty();
-    assignOptions(select, options);
-    select.val(value);
-  }
-  
-  $.fn.updateFields.widgetHandlers = {
-    'default': selectWidgetHandler
-  };
-  
-  $.fn.bindFields = function(url, fields) {
-    var handlers = $.fn.bindFields.widgetHandlers;
-    return this.each(function(index, field){
-      $(field).change(function(event, data){
-        if (data && 'triggeredByDynamicChoices' in data) return;
-        $(fields).updateFields(url, field.form);
-      }).attr(DATA_BOUND_FIELDS, fields);
-    });
-  };
-  
-  function defaultFieldNameExtractor(fieldset, field) {
-    var match = field.match(/^([\w_]+)-(\w+)-([\w_]+)$/);
-    if (match && match[1] == fieldset) {
-      return {
-        index: match[2],
-        name: match[3]
-      };
-    } else error('Can\'t resolve field "' + field + '" of specified fieldset "' + fieldset + '".');
-  }
-  
-  function defaultFieldSelectorBuilder(fieldset, field, index) {
-    return '#id_' + fieldset + '-' + index + '-' + field;
-  }
-  
-  function curryBuilder(fieldset, index, builder) {
-    return function(i, field){
-      return builder(fieldset, field, index);
+
+    $.fn.updateFields = function(url, form) {
+        var handlers = $.fn.updateFields.widgetHandlers;
+        if (this.length) {
+            form = $(form ? form : this[0].form);
+            var fields = $(this).addClass('loading'),
+                data = $(form).serializeArray(),
+                boundFields = [];
+            // Make sure fields bound to these ones are updated right after
+            fields.each(function(i, field) {
+                var selector = $(field).attr(DATA_BOUND_FIELDS);
+                if (selector) boundFields.push(selector);
+                var formset = $(field).closest('[' + DATA_FORMSET + ']').attr(DATA_FORMSET);
+                if (formset) boundFields.push($.fn.bindFieldset.formsetsBindings[formset](field));
+            });
+            fields.addClass('loading');
+            data.push({
+                name: 'DYNAMIC_CHOICES_FIELDS',
+                value: getFieldNames(fields).join(',')
+            });
+            $.getJSON(url, $.param(data), function(json) {
+                fields.each(function(index, field) {
+                    if (field.name in json) {
+                        var data = json[field.name];
+                        if (data.widget in handlers) {
+                            handlers[data.widget](field, data.value);
+                            $(field).trigger('change', {
+                                'triggeredByDynamicChoices': true
+                            });
+                        } else error('Missing handler for "' + data.widget + '" widget.');
+                    }
+                    $(field).removeClass('loading');
+                });
+                $(boundFields.join(', ')).updateFields(url, form);
+            });
+        }
+        return this;
     };
-  }
-  
-  function formsetFieldBoundFields(fieldset, field, fields, extractor, builder) {
-    field = extractor(fieldset, field.name);
-    if (field.name in fields) {
-      var selectors = $(fields[field.name]).map(curryBuilder(fieldset, field.index, builder));
-      return selectors.toArray().join(', ');
-    } else return '';
-  }
-  
-  $.fn.bindFieldset = function(url, fieldset, fields, extractor, builder) {
-    extractor = $.isFunction(extractor) ? extractor : defaultFieldNameExtractor;
-    builder = $.isFunction(builder) ? builder : defaultFieldSelectorBuilder;
-    $.fn.bindFieldset.formsetsBindings[fieldset] = function(field){
-      return formsetFieldBoundFields(fieldset, field, fields, extractor, builder);
+
+    function assignOptions(element, options) {
+        $(options).each(function(index, option) {
+            if ($.isArray(option[1])) {
+                var optGroup = $('<optgroup></optgroup>').attr({
+                    label: option[0]
+                });
+                assignOptions(optGroup, option[1]);
+                element.append(optGroup);
+            } else {
+                element.append($('<option></option>').attr({
+                    value: option[0]
+                }).html(option[1]));
+            }
+        });
+    }
+
+    function selectWidgetHandler(select, options) {
+        select = $(select);
+        var value = select.val();
+        select.empty();
+        assignOptions(select, options);
+        select.val(value);
+    }
+
+    $.fn.updateFields.widgetHandlers = {
+        'default': selectWidgetHandler
     };
-    return this.each(function(index, container){
-      $(container).change(function(event, data){
-        if (data && 'triggeredByDynamicChoices' in data) return;
-        var target = event.target,
-            selectors = formsetFieldBoundFields(fieldset, target, fields, extractor, builder);
-        $(selectors).updateFields(url, target.form);
-      }).attr(DATA_FORMSET, fieldset);
-    });
-  };
-  $.fn.bindFieldset.formsetsBindings = {};
-	
+
+    $.fn.bindFields = function(url, fields) {
+        var handlers = $.fn.bindFields.widgetHandlers;
+        return this.each(function(index, field) {
+            $(field).change(function(event, data) {
+                if (data && 'triggeredByDynamicChoices' in data) return;
+                $(fields).updateFields(url, field.form);
+            }).attr(DATA_BOUND_FIELDS, fields);
+        });
+    };
+
+    function defaultFieldNameExtractor(fieldset, field) {
+        var match = field.match(/^([\w_]+)-(\w+)-([\w_]+)$/);
+        if (match && match[1] == fieldset) {
+            return {
+                index: match[2],
+                name: match[3]
+            };
+        } else error('Can\'t resolve field "' + field + '" of specified fieldset "' + fieldset + '".');
+    }
+
+    function defaultFieldSelectorBuilder(fieldset, field, index) {
+        return '#id_' + fieldset + '-' + index + '-' + field;
+    }
+
+    function curryBuilder(fieldset, index, builder) {
+        return function(i, field) {
+            return builder(fieldset, field, index);
+        };
+    }
+
+    function formsetFieldBoundFields(fieldset, field, fields, extractor, builder) {
+        field = extractor(fieldset, field.name);
+        if (field.name in fields) {
+            var selectors = $(fields[field.name]).map(curryBuilder(fieldset, field.index, builder));
+            return selectors.toArray().join(', ');
+        } else return '';
+    }
+
+    $.fn.bindFieldset = function(url, fieldset, fields, extractor, builder) {
+        extractor = $.isFunction(extractor) ? extractor : defaultFieldNameExtractor;
+        builder = $.isFunction(builder) ? builder : defaultFieldSelectorBuilder;
+        $.fn.bindFieldset.formsetsBindings[fieldset] = function(field) {
+            return formsetFieldBoundFields(fieldset, field, fields, extractor, builder);
+        };
+        return this.each(function(index, container) {
+            $(container).change(function(event, data) {
+                if (data && 'triggeredByDynamicChoices' in data) return;
+                var target = event.target,
+                    selectors = formsetFieldBoundFields(fieldset, target, fields, extractor, builder);
+                $(selectors).updateFields(url, target.form);
+            }).attr(DATA_FORMSET, fieldset);
+        });
+    };
+    $.fn.bindFieldset.formsetsBindings = {};
 });
